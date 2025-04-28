8f7aab7e51450ed8e7099317f546a94753741983
Ralph Marvin Addo <ralphmarvin4@gmail.com>
2025-04-28 21:51:31 +0000

    feat: introduce NewsSource data model for representing news sources in the application

---
+++ composeApp/src/commonMain/kotlin/org/behemoth/dailydigest/domain/entity/NewsSource.kt
@@ -0,0 +1,11 @@
+package org.behemoth.dailydigest.domain.entity
+
+data class NewsSource(
+    val id: String,
+    val name: String,
+    val description: String,
+    val url: String,
+    val category: String,
+    val language: String,
+    val country: String
+)
