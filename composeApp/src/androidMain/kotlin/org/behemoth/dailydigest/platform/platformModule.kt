fa129ee7aca66cbc0a899578acc66c2cc59607a3
Ralph Marvin Addo <ralphmarvin4@gmail.com>
2025-04-28 21:50:36 +0000

    feat: create platformModule for platform-specific dependency injection

---
+++ composeApp/src/androidMain/kotlin/org/behemoth/dailydigest/platform/platformModule.kt
@@ -0,0 +1,8 @@
+package org.behemoth.dailydigest.di
+
+import org.koin.core.module.Module
+import org.koin.dsl.module
+
+actual fun platformModule(): Module = module {
+  // If you have any platform-specific dependencies, you can declare them here.
+}
